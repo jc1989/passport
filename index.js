7b626189783608bd9f554d9b9a7f93d65da559d0
Joshua Cannedy <jdcannedy@gmail.com>
2020-01-05 23:56:45 -0600

    passport

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const app = express();
 const models = require('./models');
 const bodyParser = require("body-parser");
 
+var pbkdf2 = require('pbkdf2');
+var salt = "4213426A433E1F9C29368F36F44F1";
+
+function encryptionPassword(password) {
+  var key = pbkdf2.pbkdf2Sync(
+    password, salt, 36000, 256, 'sha256'
+  );
+  var hash = key.toString('hex');
+
+  return hash;
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
+
 
 /*  PASSPORT SETUP  */
 
